df1c3e00fd3e1bea9780c9fec2e9fdeb0029d3f9
251658 Filip Wasiel <251658@edu.p.lodz.pl>
2025-03-25 23:46:41 +0100

    small changes

--- ConcurrentProgramming/PresentationModel/AssemblyInfo.cs
+++ ConcurrentProgramming/PresentationModel/AssemblyInfo.cs
@@ -3,5 +3,3 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("PresentationModelTest")]
 [assembly: ComVisible(false)]
-// Tutaj zmienic
-[assembly: Guid("8424f3e6-6484-4029-8743-bb762c82077a")]
--- ConcurrentProgramming/PresentationViewModel/AssemblyInfo.cs
+++ ConcurrentProgramming/PresentationViewModel/AssemblyInfo.cs
@@ -3,5 +3,3 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("PresentationViewModelTest")]
 [assembly: ComVisible(false)]
-// to zmienic
-[assembly: Guid("ddcd2a69-feed-403a-a3c1-747c71bf21ab")]
